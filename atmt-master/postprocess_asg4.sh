c9870e20a532c6dbd5cb5f1dfd26d79eff557442
Anja Koller <anja.koller@uzh.ch>
2020-11-24 15:51:29 +0100

    Add translation for beamsearch with diversity and without diversity

--- atmt-master/postprocess_asg4.sh
+++ atmt-master/postprocess_asg4.sh
@@ -1,7 +1,9 @@
-infile=model_translations_nbest.txt
-outfile=model_translations_nbest.out
+infile=model_translations_nbest_k3_alpha09_no_diversity.txt
+outfile=model_translations_nbest_k3_alpha09_no_diversity.out
 lang=en
 
 cat $infile | sed -r 's/(@@ )|(@@ ?$)//g' | perl moses_scripts/detruecase.perl | perl moses_scripts/detokenizer.perl -q -l $lang > $outfile
-cat $outfile | sacrebleu data_asg4/raw_data/test_nbest.en # computes bleuscore and n-gram precision
+# cat $outfile | sacrebleu data_asg4/raw_data/test.en # computes bleuscore and n-gram precision
 # important: here change reference path for sacrebleu to data_asg4/raw_data/test.en
+# sacrebleu commented out for the nbest approach as this makes not sense and the test and outfile are not of the same dimensionsnning1096
+
